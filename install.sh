76e28e77222f1420cab593fed677eb53e7603062
KENZIBUATRDPEEQ <127594238+KENZIBUATRDPEEQ@users.noreply.github.com>
2023-09-02 16:01:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 clear
-echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+echo -e "${GREEN}Installing Ken Proxy...${ENDCOLOR}"
 sleep 1
 if [ -f "flux" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
@@ -14,6 +14,6 @@ fi
 wget -q https://github.com/Zweaf/flux/raw/main/flux
 sleep 1
 echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
-echo -e "${GREEN}Execute proxy with this command: ./flux${ENDCOLOR}"
+echo -e "${GREEN}Execute proxy with this command: ./proxyken${ENDCOLOR}"
 chmod +x flux
 rm -r -f install.sh
